55609ca1dbfc4e4e2760794851b47ef908cb7309
HukakHePak <valsidalvonilb@outlook.com>
2021-11-13 22:00:18 +0400

    start write methods

--- Settings_Interface/Settings_Interface.cpp
+++ Settings_Interface/Settings_Interface.cpp
@@ -2,19 +2,137 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+enum DataType
+{
+	dtInteger,
+	dtFloat,
+	dtBoolean,
+	dtString,
+	dtUnknown
+};
+
+class ISettingsValue
+{
+	string value;
+	DataType type;
+
+public:
+	ISettingsValue(const string& value = "") { SetValue(value); }
+
+	void SetValue(const string& value)
+	{
+		this->value = value;
+		type = dtString;
+	}
+
+	virtual void SetValue(DataType type, const string& value)
+	{
+		this->value = value;
+		this->type = type;
+	}
+
+	virtual string AsString() { return value; }
+
+	virtual int AsInteger()
+	{
+		try { return stoi(value); }
+		catch (std::invalid_argument e)	{ return 0; }
+	}
+
+	virtual double AsDouble()
+	{
+		try { return stod(value); }
+		catch (std::invalid_argument e)	{ return 0; }		
+	}
+
+	virtual bool AsBoolean() { return !value.empty(); }
+
+	virtual DataType GetType() { return type; }
+
+	virtual ~ISettingsValue() {}
+
+};
+
+class ISettings
+{
+	string value;
+
+	struct parameter {
+		string value;
+		DataType type;
+	};
+	
+	map <string, parameter> list;
+
+public:
+
+	virtual bool LoadFromFile(const string& name)
+	{
+
+	}
+
+	virtual bool SaveToFile(const string& name) = 0;
+
+	virtual ISettingsValue Get(const string& paramName) = 0;
+
+	virtual int GetInteger(const string& paramName)
+	{
+		try { return stoi(value); }
+		catch (std::invalid_argument e) { return 0; }
+	}
+
+	virtual double GetFloat(const string& paramName)
+	{
+		try { return stod(value); }
+		catch (std::invalid_argument e) { return 0; }
+	}
+
+	virtual bool GetBoolean(const string& paramName) 
+	{ 
+		return !value.empty(); 
+	}
+
+	virtual string GetString(const string& paramName)
+	{
+		return value;
+	}
+
+	virtual void SetValue(const string& paramName, const ISettingsValue& value) = 0;
+
+	virtual void SetValue(const string& paramName, int value) = 0;
+
+	virtual void SetValue(const string& paramName, double value) = 0;
+
+	virtual void SetValue(const string& paramName, bool value) = 0;
+
+	virtual void SetValue(const string& paramName, const char* value) = 0;
+
+	virtual void SetInteger(const string& paramName, int value) = 0;
+
+	virtual void SetFloat(const string& paramName, double value) = 0;
+
+	virtual void SetBoolean(const string& paramName, bool value) = 0;
+
+	virtual void SetString(const string& paramName, const string& value) = 0;
+
+	virtual ~ISettings() {};
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	ISettingsValue a;
+	a.SetValue("");
+
+	cout << a.AsString() << endl;
+    cout << a.AsBoolean() << endl;    
+	cout << a.AsDouble () << endl;
+	cout << a.AsInteger() << endl;
+	system("pause");
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
