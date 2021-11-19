3d0c09a51cbf8f37d0763cd89d1e2d9f7d65d0a2
HukakHePak <valsidalvonilb@outlook.com>
2021-11-19 16:34:21 +0400

    app to lib

---
+++ Settings_Interface/ISettings.h
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+namespace ISettings
+{
+	enum DataType
+	{
+		dtInteger,
+		dtFloat,
+		dtBoolean,
+		dtString,
+		dtUnknown
+	};
+
+	class ISettingsValue
+	{
+		string value = "";
+		DataType type = dtUnknown;
+
+	public:
+
+		virtual void SetValue(const string& value) = 0;
+
+		virtual void SetValue(DataType type, const string& value) = 0;
+
+		virtual string AsString() = 0;
+
+		virtual int AsInteger() = 0;
+
+		virtual double AsDouble() = 0;
+
+		virtual bool AsBoolean() = 0;
+
+		virtual DataType GetType() = 0;
+
+		virtual ~ISettingsValue() {};
+	};
+
+	class ISettings
+	{
+		struct parameter
+		{
+			string value = "";
+			DataType type = dtUnknown;
+		};
+
+		map <string, parameter> list;
+
+		virtual void editParamInList(const string& paramName, const string& value, DataType type) = 0;
+
+		virtual bool isNumber(char symbol) = 0;
+
+		virtual bool isValIdSymbol(char symbol) = 0;
+
+		virtual string filterName(const string& name) = 0;
+
+		virtual string paramToStrForSave(const parameter& param) = 0;
+
+		virtual DataType checkStrType(const string& str) = 0;
+
+		virtual string parseStrByType(const string& str, DataType type) = 0;
+
+	public:
+
+		virtual bool LoadFromFile(const string& name) = 0;
+
+		virtual bool SaveToFile(const string& name) = 0;
+
+		virtual ISettingsValue* Get(const string& paramName) = 0;
+
+		virtual int GetInteger(const string& paramName) = 0;
+
+		virtual double GetFloat(const string& paramName) = 0;
+
+		virtual bool GetBoolean(const string& paramName) = 0;
+
+		virtual string GetString(const string& paramName) = 0;
+
+		virtual void SetValue(const string& paramName, ISettingsValue& value) = 0;
+
+		virtual void SetValue(const string& paramName, int value) = 0;
+
+		virtual void SetValue(const string& paramName, double value) = 0;
+
+		virtual void SetValue(const string& paramName, bool value) = 0;
+
+		virtual void SetValue(const string& paramName, const char* value) = 0;
+
+		virtual void SetInteger(const string& paramName, int value) = 0;
+
+		virtual void SetFloat(const string& paramName, double value) = 0;
+
+		virtual void SetBoolean(const string& paramName, bool value) = 0;
+
+		virtual void SetString(const string& paramName, const string& value) = 0;
+
+		virtual ~ISettings() {};
+	};
+}
